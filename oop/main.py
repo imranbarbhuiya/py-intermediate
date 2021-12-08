7fdd6f9da405a5e67f4a9c82550d476f2ff2e4a5
Parbez <74945038+imranbarbhuiya@users.noreply.github.com>
2021-12-08 13:14:13 +0530

    int python

---
+++ oop/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen, Turtle
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral", "yellow")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
